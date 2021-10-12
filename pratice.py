094c7f1ee68e20b9940d08ae28349889fd4be7dd
박준오 <80104526+VinChaud@users.noreply.github.com>
2021-10-13 08:13:47 +0900

    Add files via upload

---
+++ pratice.py
@@ -0,0 +1,25 @@
+a = True 
+b = False
+print(a,b)
+lastname = '박'
+firstname = '준오'
+print(lastname,firstname)
+
+# 더하기 + , 나누기 / , 곱하기 * , 빼기 - , 거듭제곱 ** , 나머지 계산 %
+print(5+5/2*2)
+print(2**3)
+print(10%3)
+
+#연결연산자 : + 
+color = '노랑색'
+mycolor = '나는' + color + '을 좋아합니다'
+print(mycolor)
+line = '-'*30
+print(line)
+string = 'Every day dog has its day'
+print(string[0:5])
+
+#글자의 개수 len()
+print(len(string))
+animal = input('좋아하는 동물을 입력하세요:')
+print(animal)
